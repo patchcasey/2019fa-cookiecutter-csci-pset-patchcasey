1fd7e3802a51bbfe13fce5915016ef49a69c4e72
patchcasey <patchcasey@gmail.com>
2019-10-17 21:56:54 -0400

    added additional template testing

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -30,10 +30,19 @@ def bake_in_temp_dir(cookies, *args, **kwargs):
     finally:
         rmtree(str(result.project))
 		
+def run_inside_dir(command, dirpath):
+    """
+    Run a command from inside a given directory, returning the exit status
+    :param command: Command that will be executed
+    :param dirpath: String, path of the directory the command is being run.
+    """
+    with inside_dir(dirpath):
+        return subprocess.check_call(shlex.split(command))
+		
 def test_bake_and_run_tests(cookies):
     with bake_in_temp_dir(cookies) as result:
         assert result.project.isdir()
-        run_inside_dir('python setup.py test', str(result.project)) == 0
+        run_inside_dir('pytest', str(result.project)) == 0
         print("test_bake_and_run_tests path", str(result.project))
 
 
