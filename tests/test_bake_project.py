98c004625fc333f1f2b8fd53951634ec3ebaec8f
patchcasey <patchcasey@gmail.com>
2019-10-17 22:11:07 -0400

    changed test to bake with defaults

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -2,7 +2,7 @@ from contextlib import contextmanager
 
 import os
 import sys
-import subprocess
+import subprocess	
 import shlex
 from cookiecutter.utils import rmtree
 
@@ -33,20 +33,16 @@ def bake_in_temp_dir(cookies, *args, **kwargs):
     finally:
         rmtree(str(result.project))
 		
-def run_inside_dir(command, dirpath):
-    """
-    Run a command from inside a given directory, returning the exit status
-    :param command: Command that will be executed
-    :param dirpath: String, path of the directory the command is being run.
-    """
-    with inside_dir(dirpath):
-        return subprocess.check_call(shlex.split(command))	
-		
-def test_bake_and_run_tests(cookies):
+def test_bake_with_defaults(cookies):
     with bake_in_temp_dir(cookies) as result:
         assert result.project.isdir()
-        run_inside_dir('pytest', str(result.project)) == 0
-        print("test_bake_and_run_tests path", str(result.project))
+        assert result.exit_code == 0
+        assert result.exception is None
+
+        found_toplevel_files = [f.basename for f in result.project.listdir()]
+        assert 'Pipfile' in found_toplevel_files
+        assert 'test_pset' in found_toplevel_files
+        assert 'pytest.ini' in found_toplevel_files
 
 
 def test_project_tree(cookies):
