3c0340b50860a038bba154a68a29ee0be0e0fff3
patchcasey <patchcasey@gmail.com>
2019-10-17 21:51:24 -0400

    added comments throughout files

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,12 @@
 if __name__ == "__main__":
 
     if "{{cookiecutter.install_csci_utils}}" == "no":
+        # this is a conditional way to make sure csci_utils is not installed if indicated not to
         with open("Pipfile", "r") as f:
             lines = f.readlines()
         # below list comprehension adapted from: https://stackoverflow.com/questions/19286657/index-all-except-one-item-in-python
-        lines_to_write = [x for i, x in enumerate(lines) if i != 10]
+        lines_to_write = [x for x in lines if x.find("csci-utils") != 0]
         with open("Pipfile", "w") as f:
+            # this just rewrites the file but excludes the line that involves csci_utils
             for line in lines_to_write:
                 f.write(line)
